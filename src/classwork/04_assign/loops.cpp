bbfd7707b536d5bde890b56b77edc4abfc3ab892
FeliciaElmer <felicia.elmer@g.austincc.edu>
2020-10-03 03:46:15 +0000

    Classwork 04 - for hw 3.  I kept getting result: not found.

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,17 @@
 //cpp
+#include "loops.h"
+#include <iostream>
+
+int factorial(int num)
+{
+  int i = 1;
+  int total = 1;
+
+  while(i <= num)
+  {
+    total = total * i;
+    i++;
+  }
+  
+  return total;
+}
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,9 @@
 //write includes statements
+#include <iostream>
+#include "loops.h"
 
 //write using statements for cin and cout
-
+using std::cout; using std::cin;
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +12,23 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
+  int num;
+  int result;
+  num = 0;
+  cout<<"Insert a number between 0 and 10 to factorial: ";
+  cin>>num;
+
+  do
+  {
+    cout<<"Insert a number between 0 and 10: "<<"\n";
+    cin>> num;
+    result = factorial(num);
+    cout<<"The factorial is: "<<result<<"\n";
+    cout<<"If you wish to end the program do not enter a number between 0 and 10. "<<"\n";
+    cin>>num;
+
+  }
+  while (num>= 0 && num <= 10);
 
 	return 0;
 }
