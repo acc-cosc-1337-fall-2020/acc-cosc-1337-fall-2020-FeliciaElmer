ab7673c080f8d05f5814ff6af472ba028b0d7192
FeliciaElmer <felicia.elmer@g.austincc.edu>
2020-10-09 00:52:28 +0000

    Homework 04 - DNA

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -1,4 +1,6 @@
 #include "dna.h"
+using namespace std;
+
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -7,15 +9,36 @@ Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
 
-
-
+double get_gc_content(std::string dna)
+{
+  int gccounter = 0;
+  for(std::size_t i = 0; i < dna.size(); ++i)
+  {
+    if (dna[i] == 'C' || dna[i] == 'G')
+    {
+      gccounter ++;
+    }
+  }
+  return gccounter / (double)dna.size();
+}
 
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
-
-
+std::string get_reverse_string(std::string dna)
+{
+  int length = dna.size();
+  int n = length-1;
+  int i = 0;
+  while(i<=n)
+  {
+    swap(dna[i], dna[n]);
+    n = n-1;
+    i = i+1;
+  }
+  return dna;
+}
 
 /*
 Write prototype for function get_dna_complement that
@@ -27,4 +50,40 @@ b. iterate local string variable and
 c. return string
 
 */
+std::string get_dna_complement(std::string dna)
+{
+  int length = dna.size();
+  int n = length-1;
+  int i = 0;
+  while(i<=n)
+  {
+    swap(dna[i], dna[n]);
+    n = n-1;
+    i = i+1;
+  }
 
+  for(std::size_t i=0; i< dna.size(); ++i)
+  {
+    if ('A' == dna[i])
+    {
+      dna[i] = 'T';
+    }
+    else if ('T'== dna[i])
+    {
+      dna[i] = 'A';
+    }
+    else if ('C' == dna[i])
+    {
+      dna[i] = 'G';
+    }
+    else if('G' == dna[i])
+    {
+      dna[i] = 'C';
+    }
+    else
+    {
+      dna[i] = '-';
+    }
+  }
+  return dna;
+}
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,11 @@
 //write include statements
+#include "dna.h"
+#include <iostream>
+#include <string>
+
 
 //write using statements
-
+using namespace std; using std::cout; using std::cin;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +16,46 @@ user enters a y or Y.
 */
 int main() 
 {
-	return 0;
+  char cont;
+  int prompt;
+  std::string dna;
+  double result_gc;
+  std::string result_complement;
+
+  cout<<"If you would like to find either the gc content or the DNA complement type y or Y: ";
+  cin>>cont;
+
+  while(cont == 'y' || cont == 'Y')
+  {
+    cout<<"Please enter 1 for the gc content or 2 for the DNA complement: ";
+	  cin>>prompt;
+    if (prompt == 1)
+    {
+      cout<<"Please enter the DNA string: ";
+      cin>>dna;
+      result_gc = get_gc_content(dna);
+      cout<<"The GC content is: "<<result_gc<<'\n';
+      cout<<"Would you like to find another gc content or a DNA complement? ";
+      cin>>cont;
+    }
+
+    else if (prompt == 2)
+    {
+      cout<<"Please enter the DNA string: ";
+      cin>>dna;
+      result_complement = get_dna_complement(dna);
+      cout<<"The complement is: "<<result_complement<<'\n';
+      cout<<"Would you like to find another gc content or a DNA complement? ";
+      cin>>cont;
+    }
+
+    else
+    {
+      cout<<"Would you like to find the gc content or a DNA complement? ";
+      cin>>cont;
+    }
+  }
+
+
+  return 0;
 }
