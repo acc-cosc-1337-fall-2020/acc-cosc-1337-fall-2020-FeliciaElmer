f65c6ec60799201e4e22d09e5dd0a512ba1adfd8
FeliciaElmer <felicia.elmer@g.austincc.edu>
2020-10-25 03:12:02 +0000

    Homework 5

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,26 @@
+#include "tic_tac_toe.h"
+
+using std::cout; using std::cin;
 
 int main() 
 {
+  int marker;
+  std::string player;
+  TicTacToe game;
 
+  cout<<"Enter a capital X or O for your sign: ";
+  cin>>player;
+
+  game.start_game(player);
+
+  while(game.game_over() == false)
+  {
+    cout<< "Choose where you'd like to place your position (1-9): ";
+    cin>> marker;
+    game.mark_board(marker);
+    game.display_board();
+  }
+
+  cout<<"The game has finished.";
 	return 0;
 }
--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,28 @@
 //h
+
+#include<iostream>
+#include<vector>
+#include<string>
+
+using std::vector; using std::string;
+
+#ifndef TicTacToe_h
+#define TicTacToe_h
+
+class TicTacToe
+{
+  public:
+    bool game_over();
+    void start_game(std::string first_player);
+    void mark_board(int position);
+    std::string get_player()const;
+    void display_board()const;
+
+  private:
+    void set_next_player();
+    bool check_board_full()const;
+    void clear_board();
+    string player;
+    vector<string> pegs{9," "};
+}
+#endif
